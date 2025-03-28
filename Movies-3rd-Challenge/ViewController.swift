15a4c8d2c5e210f944662ecaf12ab77335c3eb06
latronixo <pamerra@mail.ru>
2025-03-28 16:16:43 +0300

    Initial Commit

---
+++ Movies-3rd-Challenge/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Movies-3rd-Challenge
+//
+//  Created by Валентин Картошкин on 28.03.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
